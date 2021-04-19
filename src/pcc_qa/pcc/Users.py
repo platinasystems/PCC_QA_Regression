039fdb8ca38b2304f6892357a81e2239ca326a0e
ANURAG JAIN <46484628+AnuragJ05@users.noreply.github.com>
2021-04-19 14:30:23 +0530

    changes by anurag

--- src/pcc_qa/pcc/Users.py
+++ src/pcc_qa/pcc/Users.py
@@ -59,7 +59,7 @@ class Users(PccBase):
         Create User Password
         """
         self._load_kwargs(kwargs)
-        banner("PCC.Add User [token=%s]" % self.Token)
+        banner("PCC.Add User [password=%s]" % self.Password)
         conn = BuiltIn().get_variable_value("${password_token}")
 
         payload = {"password": self.Password}
