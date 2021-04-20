472fc8afc532ab1891767db8e2eddea836b7280e
calsoftAbinash <Abinash.Patnaik@calsoftinc.com>
2021-04-19 23:56:20 -0700

    Replaced uninstall pcc_qa with uninstall PCC_QA_Regression

--- build-pcc_qa.sh
+++ build-pcc_qa.sh
@@ -5,5 +5,5 @@ fi
 echo "MOTOR_HOME=$MOTOR_HOME"
 cd $MOTOR_HOME
 sudo -H python3 setup.py sdist
-sudo -H pip3 uninstall -y pcc_qa
+sudo -H pip3 uninstall -y PCC_QA_Regression
 sudo -H pip3 install $MOTOR_HOME/dist/PCC_QA_Regression-1.0.0.tar.gz
