6ca9d9a403a96b312872f159d8df70bc4d70722f
ANURAG JAIN <46484628+AnuragJ05@users.noreply.github.com>
2021-04-23 11:13:57 +0530

    Modified tenant user creation and feature version code

--- src/pcc_qa/pcc/Tenants.py
+++ src/pcc_qa/pcc/Tenants.py
@@ -299,10 +299,10 @@ class Tenants(PccBase):
             trace("tenant_names: {}".format(tenant_names))
             if tenant_names == ['ROOT']:
                 return "OK"
-            elif tenant_names == ['ROOT','Tenant_6']:
+            elif tenant_names == ['ROOT']:
                 return "OK"
             else:
-                tenants_to_be_deleted = set(tenant_names)- set(['ROOT','Tenant_6'])
+                tenants_to_be_deleted = set(tenant_names)- set(['ROOT'])
                 trace("tenants_to_be_deleted: {}".format(tenants_to_be_deleted))
                 for tenant in list(tenants_to_be_deleted):
                     trace("Deleting tenant: {}".format(tenant))
@@ -311,7 +311,7 @@ class Tenants(PccBase):
                     if response['StatusCode'] == 200:
                         continue
                     else:
-                        return Error
+                        return "Error"
                 return "OK"
         except Exception as e:
             return {"Error: {}".format(e)} 
