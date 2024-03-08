25926ed59a6efc7c146b69ba0d5af9205670961e
alexlocastro <alex.locastro@live.it>
2024-03-08 14:32:31 +0100

    test 1

--- src/pcc_qa/scripts/run-pcc_qa.sh
+++ src/pcc_qa/scripts/run-pcc_qa.sh
@@ -6,4 +6,4 @@
 #cd $MOTOR_HOME
 export PYTHONPATH=/usr/local/robot
 #robot -x $MOTOR_HOME/output/robot.xml --outputdir $MOTOR_HOME/output $1 $2 $3 $4 $5 $6 $7 $8
-robot -x output/robot.xml --outputdir output
+robot -x output/robot.xml --outputdir output ${TEST_NAME}
