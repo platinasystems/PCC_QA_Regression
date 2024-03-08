3bad0bb43335403d0e30c219e66547112574ddfd
alexlocastro <alex.locastro@live.it>
2024-03-08 14:31:28 +0100

    test 1

--- src/pcc_qa/scripts/run-pcc_qa.sh
+++ src/pcc_qa/scripts/run-pcc_qa.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ -z "$MOTOR_HOME" ]; 
-  then export MOTOR_HOME="/PCC_QA_Regression";
-fi
-echo "MOTOR_HOME=$MOTOR_HOME"
-cd $MOTOR_HOME
+#if [ -z "$MOTOR_HOME" ];
+#  then export MOTOR_HOME="/PCC_QA_Regression";
+#fi
+#echo "MOTOR_HOME=$MOTOR_HOME"
+#cd $MOTOR_HOME
 export PYTHONPATH=/usr/local/robot
-robot -x $MOTOR_HOME/output/robot.xml --outputdir $MOTOR_HOME/output $1 $2 $3 $4 $5 $6 $7 $8
+#robot -x $MOTOR_HOME/output/robot.xml --outputdir $MOTOR_HOME/output $1 $2 $3 $4 $5 $6 $7 $8
+robot -x output/robot.xml --outputdir output
